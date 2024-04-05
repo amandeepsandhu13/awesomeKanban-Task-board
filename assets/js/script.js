632dcbdeddb0b09c8911b3ee3828b320c1ad7733
Amandeep <samandeepkaur34@gmail.com>
2024-04-04 21:46:59 -0400

    call printProjectData on document load

--- assets/js/script.js
+++ assets/js/script.js
@@ -241,5 +241,7 @@ projectDisplayEl.on('click', '.btn-delete-project', handleDeleteTask);
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
+  printProjectData();
+
 });
 
